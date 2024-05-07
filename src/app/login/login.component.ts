7b206db985f46f144c17b7db0e8c374ab612348c
Fernanda Miyuki <miyukifernanda26@gmail.com>
2024-05-07 10:24:36 -0300

    adicionando os arquivos iniciais com login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  credentials = { email:'', senha:''};
+  errorMessage = '';
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+
+
+  cadastrar() {
+    this.router.navigate(['/register']);
+    
+  }
+}

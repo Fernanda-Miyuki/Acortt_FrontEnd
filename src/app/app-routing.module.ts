ccbf158726411816f1b4c47472b86c054f6ec268
Jeferson <jgiovanimelo@gmail.com>
2024-05-15 11:19:18 -0300

    Telapesquisa estilizada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
+import { TelapesquisaComponent } from './telapesquisa/telapesquisa.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home' , pathMatch: 'full'},
   { path: 'home', component:  HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component:  RegisterComponent },
-  { path: 'profile', component: ProfileComponent}
+  { path: 'profile', component: ProfileComponent},
+  { path: 'telapesquisa', component: TelapesquisaComponent}
 ];
 
 @NgModule({

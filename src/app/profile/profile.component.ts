b9f05056d754d8e7038d9a310f9cef8336a76435
Fernanda Miyuki <miyukifernanda26@gmail.com>
2024-05-15 22:49:03 -0300

    Iniciando a criação da tela Profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-publicar',
+  selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+  constructor(private router: Router) {}
 
-}
+
+  anuncio = [
+    {
+      urlImage:
+      '/assets/anh-nhat-PdALQmfEqvE-unsplash.jpg',    
+      titulo: 'R$ 5000,00',
+      subtitulo: 'Placa mãe',
+      local: 'Fatec Shunji Nishimura de Pompeia',
+      data: '10/05/2024 - 10:00',
+    },
+    {
+      urlImage:
+      '/assets/simerpreet-singh-OOVomt_6wEY-unsplash.jpg',
+      titulo: 'R$ 1000,00',
+      subtitulo: 'Celular A20',
+      local: 'Fatec Shunji Nishimura de Pompeia',
+      data: '10/05/2024 - 10:00',
+    },
+    {
+      urlImage:
+      '/assets/tim-mossholder-VOXs79vySFo-unsplash (1).jpg',
+      titulo: 'R$ 12000,00',
+      subtitulo: 'Iphone 16',
+      local: 'Fatec Shunji Nishimura de Pompeia',
+      data: '10/05/2024 - 10:00',
+    },
+    {
+      urlImage:
+      '/assets/simerpreet-singh-OOVomt_6wEY-unsplash.jpg',
+      titulo: 'R$ 3000,00',
+      subtitulo: 'Samsung Galaxy',
+      local: 'Fatec Shunji Nishimura de Pompeia',
+      data: '10/05/2024 - 10:00',
+    },
+    {
+      urlImage:
+      '/assets/simerpreet-singh-OOVomt_6wEY-unsplash.jpg',
+      titulo: 'R$ 3000,00',
+      subtitulo: 'Samsung Galaxy',
+      local: 'Fatec Shunji Nishimura de Pompeia',
+      data: '10/05/2024 - 10:00',
+    },
+    {
+      urlImage:
+      '/assets/simerpreet-singh-OOVomt_6wEY-unsplash.jpg',
+      titulo: 'R$ 3000,00',
+      subtitulo: 'Samsung Galaxy',
+      local: 'Fatec Shunji Nishimura de Pompeia',
+      data: '10/05/2024 - 10:00',
+    },
+   ];
+  } 

993d4407aede70d5f9437fccdbacfe819e06e2e0
Jeferson <jgiovanimelo@gmail.com>
2024-05-21 09:16:31 -0300

    Carrossel adicionado no projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NavbarlogComponent } from './navbarlog/navbarlog.component';
 import { TelapesquisaComponent } from './telapesquisa/telapesquisa.component';
 import { TelamodalComponent } from './telamodal/telamodal.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SliderComponent } from './slider/slider.component';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     NavbarComponent,
     TelapesquisaComponent,
     NavbarlogComponent,
-    TelamodalComponent
+    TelamodalComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,

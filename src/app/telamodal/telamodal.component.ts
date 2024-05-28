522b057e4ec62a36ce001fd535f7b11be38af177
Fernanda Miyuki <miyukifernanda26@gmail.com>
2024-05-28 11:17:14 -0300

    Tela modal completa

--- src/app/telamodal/telamodal.component.ts
+++ src/app/telamodal/telamodal.component.ts
@@ -1,5 +1,5 @@
-
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-telamodal',
@@ -7,35 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./telamodal.component.scss']
 })
 export class TelamodalComponent {
-  nomeProduto: string = '';
-  categoriaProduto: string = '';
-  valorProduto: string = '';
-  descricaoProduto: string = '';
-  fileList: File[] = [];
+  constructor(
+    private modalService: NgbModal
+  ) {}
 
-  onFileSelect(event: any) {
-    const files = event.target.files;
-    for (let i = 0; i < files.length; i++) {
-      this.fileList.push(files[i]);
-    }
-  }
-
-  removeFile(index: number) {
-    this.fileList.splice(index, 1);
-  }
-
-  onSubmit() {
-    // Coleta os dados do formulário:
-    console.log('Nome do Produto:', this.nomeProduto);
-    console.log('Categoria:', this.categoriaProduto);
-    console.log('Valor:', this.valorProduto);
-    console.log('Descrição:', this.descricaoProduto);
-    console.log('Arquivos:', this.fileList);
-
-    // Faça o que você precisa com os dados, como enviá-los para um servidor.
-  }
-
-  closeModal() {
-    // Implemente a lógica para fechar o modal.
+  closeModal():void {
+    this.modalService.dismissAll();
   }
 }

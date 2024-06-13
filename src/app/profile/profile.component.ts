39c7b2309392ad4921c18064da256d56523ee3c2
Fernanda Miyuki <miyukifernanda26@gmail.com>
2024-06-13 10:57:15 -0300

    Atualizações do card de profile e navbarlog com novo user

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -17,53 +17,48 @@ export class ProfileComponent {
 
   anuncio = [
     {
-      urlImage:
-      '/assets/anh-nhat-PdALQmfEqvE-unsplash.jpg',    
-      titulo: 'Placa mãe',
-      subtitulo: 'R$ 5000,00',
-      local: 'Fatec Shunji Nishimura de Pompeia',
-      data: '10/05/2024 - 10:00',
-    },
+        urlImage: '/assets/anh-nhat-PdALQmfEqvE-unsplash.jpg',
+        titulo: 'Smartphone Samsung Galaxy',
+        subtitulo: 'R$ 5000,00',
+        local: 'Fatec',
+        data: '10/05/2024 - 10:00',
+      },
     {
-      urlImage:
-      '/assets/simerpreet-singh-OOVomt_6wEY-unsplash.jpg',
-      titulo: 'Samsung A20',
-      subtitulo: 'R$ 1000,00',
-      local: 'Fatec Shunji Nishimura de Pompeia',
-      data: '10/05/2024 - 10:00',
-    },
+        urlImage: '/assets/simerpreet-singh-OOVomt_6wEY-unsplash.jpg',
+        titulo: 'Smartphone Samsung Galaxy',
+        subtitulo: 'R$ 1000,00',
+        local: 'Fatec',
+        data: '10/05/2024 - 10:00',
+      },
     {
-      urlImage:
-      '/assets/tim-mossholder-VOXs79vySFo-unsplash (1).jpg',
-      titulo: 'Iphone 16',
-      subtitulo: 'R$ 12000,00',
-      local: 'Fatec Shunji Nishimura de Pompeia',
-      data: '10/05/2024 - 10:00',
-    },
+        urlImage: '/assets/simerpreet-singh-OOVomt_6wEY-unsplash.jpg',
+        titulo: 'Smartphone Samsung Galaxy',
+        subtitulo: 'R$ 1000,00',
+        local: 'Fatec',
+        data: '10/05/2024 - 10:00',
+      },
     {
-      urlImage:
-      '/assets/simerpreet-singh-OOVomt_6wEY-unsplash.jpg',
-      titulo: 'Motorola G Pro',
-      subtitulo: 'R$ 3000,00',
-      local: 'Fatec Shunji Nishimura de Pompeia',
-      data: '10/05/2024 - 10:00',
-    },
+        urlImage: '/assets/simerpreet-singh-OOVomt_6wEY-unsplash.jpg',
+        titulo: 'Smartphone Samsung Galaxy',
+        subtitulo: 'R$ 1000,00',
+        local: 'Fatec',
+        data: '10/05/2024 - 10:00',
+      },
     {
-      urlImage:
-      '/assets/simerpreet-singh-OOVomt_6wEY-unsplash.jpg',
-      titulo: 'Redmi 12',
-      subtitulo: 'R$ 3000,00',
-      local: 'Fatec Shunji Nishimura de Pompeia',
-      data: '10/05/2024 - 10:00',
-    },
+        urlImage: '/assets/simerpreet-singh-OOVomt_6wEY-unsplash.jpg',
+        titulo: 'Smartphone Samsung Galaxy',
+        subtitulo: 'R$ 1000,00',
+        local: 'Fatec',
+        data: '10/05/2024 - 10:00',
+      },
     {
-      urlImage:
-      '/assets/simerpreet-singh-OOVomt_6wEY-unsplash.jpg',
-      titulo: 'Samsung Galaxy',
-      subtitulo: 'R$ 3000,00',
-      local: 'Fatec Shunji Nishimura de Pompeia',
-      data: '10/05/2024 - 10:00',
-    },
+        urlImage: '/assets/simerpreet-singh-OOVomt_6wEY-unsplash.jpg',
+        titulo: 'Smartphone Samsung Galaxy',
+        subtitulo: 'R$ 1000,00',
+        local: 'Fatec',
+        data: '10/05/2024 - 10:00',
+      }
+
    ];
 
    openModal():void{
